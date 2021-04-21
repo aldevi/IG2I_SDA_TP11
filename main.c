170232465de418d5e77d40373daa344d08e44694
Alexis Deville <alexis.deville@ig2i.centralelille.fr>
2021-04-21 09:25:53 +0200

    Correction bug

--- main.c
+++ main.c
@@ -1,72 +1,81 @@
-#include"ligue.h"
+#include "ligue.h"
 
 void testLigue();
 void testEquipe();
 void testMatch();
 
-int main(){
-	//testEquipe();
-	//testMatch();
+int main()
+{
+	testEquipe();
+	testMatch();
 	//testLigue();
 
 	return 0;
 }
 
-
-void testEquipe(){
-	T_equipe e1 = {"LOSC",14,8,3,3,37,24,0};
-	T_equipe e2 = {"RCL",14,6,5,3,25,23,0};
-	T_equipe e3 = {"OM",14,6,5,3,30,28,0};
-	T_equipe *ptrEA,*ptrEB;
+void testEquipe()
+{
+	T_equipe e1 = {"LOSC", 14, 8, 3, 3, 37, 24, 0};
+	T_equipe e2 = {"RCL", 14, 6, 5, 3, 25, 23, 0};
+	T_equipe e3 = {"OM", 14, 6, 5, 3, 30, 28, 0};
+	T_equipe *ptrEA, *ptrEB;
 	printf("Avant MÀJ des points:\n");
-	afficherEquipe(&e1,'V',1);
+	afficherEquipe(&e1, 'V', 1);
 	miseAJourPoints(&e1);
 	printf("\nAprès MÀJ des points:\n");
-	afficherEquipe(&e1,'H',1);
+	afficherEquipe(&e1, 'H', 1);
 
 	miseAJourPoints(&e2);
 	miseAJourPoints(&e3);
 
-	printf("\nMoyenne de points par match de %s: %f\n",e2.nom,moyPointsMatch(&e2));
+	printf("\nMoyenne de points par match de %s: %f\n", e2.nom, moyPointsMatch(&e2));
 
-	ptrEA = &e2; ptrEB = &e3;
-	printf("\nComparaison entre %s et %s: ",ptrEA->nom,ptrEB->nom);
-	int cmp = comparer(ptrEA,ptrEB);
-	if(cmp>0)
-		printf("%s est meilleure\n",ptrEA->nom);
-	else if(cmp<0)
-		printf("%s est meilleure\n",ptrEB->nom);
+	ptrEA = &e2;
+	ptrEB = &e3;
+	printf("\nComparaison entre %s et %s: ", ptrEA->nom, ptrEB->nom);
+	int cmp = comparer(ptrEA, ptrEB);
+	if (cmp > 0)
+		printf("%s est meilleure\n", ptrEA->nom);
+	else if (cmp < 0)
+		printf("%s est meilleure\n", ptrEB->nom);
 	else
 		printf("ex aequo !\n");
-
 }
 
-void testMatch(){
-	T_equipe e1 = {"LOSC",14,8,3,3,37,24,0};
-	T_equipe e2 = {"RCL",14,6,5,3,25,23,0};
-	T_equipe e3 = {"OM",14,6,5,3,30,28,0};
-	miseAJourPoints(&e1); miseAJourPoints(&e2); miseAJourPoints(&e3);
+void testMatch()
+{
+	T_equipe e1 = {"LOSC", 14, 8, 3, 3, 37, 24, 0};
+	T_equipe e2 = {"RCL", 14, 6, 5, 3, 25, 23, 0};
+	T_equipe e3 = {"OM", 14, 6, 5, 3, 30, 28, 0};
+	miseAJourPoints(&e1);
+	miseAJourPoints(&e2);
+	miseAJourPoints(&e3);
 
-	T_match m1 = {&e1,&e2,-1,-1};
-	T_match m2 = {&e3,&e1,-1,-1};
+	T_match m1 = {&e1, &e2, -1, -1};
+	T_match m2 = {&e3, &e1, -1, -1};
 
 	afficherMatch(&m1);
 	afficherMatch(&m2);
-	afficherEquipe(&e1,'H',1); afficherEquipe(&e2,'H',0); afficherEquipe(&e3,'H',0);
+	afficherEquipe(&e1, 'H', 1);
+	afficherEquipe(&e2, 'H', 0);
+	afficherEquipe(&e3, 'H', 0);
 
-	jouer(&m1,2,1);
+	jouer(&m1, 2, 1);
 	jouerAleatoire(&m2);
 	afficherMatch(&m1);
 	afficherMatch(&m2);
-	afficherEquipe(&e1,'H',1); afficherEquipe(&e2,'H',0); afficherEquipe(&e3,'H',0);
+	afficherEquipe(&e1, 'H', 1);
+	afficherEquipe(&e2, 'H', 0);
+	afficherEquipe(&e3, 'H', 0);
 }
 
-void testLigue(){
+void testLigue()
+{
 	T_ligue ligue;
-	chargerEquipes("tableau.csv",&ligue);
+	chargerEquipes("tableau.csv", &ligue);
 	printf("Tableau après chargement...\n");
 	afficherTableau(&ligue);
-	simulation(&ligue,21);
+	simulation(&ligue, 21);
 	printf("Tableau après simulation...\n");
 	afficherTableau(&ligue);
 	printf("Tirage au sort:\n");
--- match.c
+++ match.c
@@ -1,43 +1,51 @@
 #include "match.h"
+#include "equipe.h"
 
-void jouer(T_match *m, int bLoc, int bVis){
-	
+void jouer(T_match *m, int bLoc, int bVis)
+{
+
 	// On met à jour les données du match
 	m->butsLoc = bLoc;
 	m->butsVis = bVis;
 
 	// On met à jour les données des équipes
 	// Local
-	m->loc->j += 1; // maj : Nombre de matchs joués
-	m->loc->butsPour += bLoc; // maj : Nombre de buts marqués
+	m->loc->j += 1;				// maj : Nombre de matchs joués
+	m->loc->butsPour += bLoc;	// maj : Nombre de buts marqués
 	m->loc->butsContre += bVis; // maj : Nombre de buts concédés
 
 	// Visiteur
-	m->vis->j += 1; // maj : Nombre de matchs joués
-	m->vis->butsPour += bVis; // maj : Nombre de buts marqués
+	m->vis->j += 1;				// maj : Nombre de matchs joués
+	m->vis->butsPour += bVis;	// maj : Nombre de buts marqués
 	m->vis->butsContre += bLoc; // maj : Nombre de buts concédés
 
 	// Mise à jour victoire et défaite
 
 	// Les locaux ont gagné le match
-	if (bLoc > bVis) {
+	if (bLoc > bVis)
+	{
 		m->loc->v += 1; // maj : Nombre victoire local
 		m->vis->p += 1; // maj : Nombre défaite visiteur
 	}
 	// Il y a égalité
-	else if (bLoc == bVis){
+	else if (bLoc == bVis)
+	{
 		m->loc->n += 1; // maj : Nombre match nul local
 		m->vis->n += 1; // maj : Nombre match nul visiteur
 	}
 	// Les locaux ont perdu
-	else if (bLoc < bVis){
+	else if (bLoc < bVis)
+	{
 		m->loc->p += 1; // maj : Nombre défaite local
 		m->vis->v += 1; // maj : Nombre victoire visiteur
 	}
 
+	miseAJourPoints(m->loc);
+	miseAJourPoints(m->vis);
 }
 
-void jouerAleatoire(T_match *m){
+void jouerAleatoire(T_match *m)
+{
 	srand(time(NULL));
 	int bLoc = rand() % MAXBUTS;
 	int bVis = rand() % MAXBUTS;
@@ -45,9 +53,10 @@ void jouerAleatoire(T_match *m){
 	jouer(m, bLoc, bVis);
 }
 
-void afficherMatch(const T_match *m){
-	if(m->butsVis < 0 || m->butsLoc <0)
-		printf("\n%s # %s (match à jouer)\n",m->loc->nom,m->vis->nom);
+void afficherMatch(const T_match *m)
+{
+	if (m->butsVis < 0 || m->butsLoc < 0)
+		printf("\n%s # %s (match à jouer)\n", m->loc->nom, m->vis->nom);
 	else
-		printf("\n%s %d # %d %s\n",m->loc->nom,m->butsLoc,m->butsVis,m->vis->nom);
+		printf("\n%s %d # %d %s\n", m->loc->nom, m->butsLoc, m->butsVis, m->vis->nom);
 }
